9d9e1e58a53b4e6cdac0e097acb71c28181c3370
riffon3000 <riffon@gmail.com>
2018-11-21 17:38:23 -0600

    db/schema.sql seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Memphis BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('OMG 2.0');
+INSERT INTO burgers (burger_name) VALUES ('Burger 101');
+INSERT INTO burgers (burger_name, devour) VALUES ('Chili Cheese Crunch', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Patty Melt', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Loaded Spud', true);
